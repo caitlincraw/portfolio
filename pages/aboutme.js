41874a44b3e0ecb94730a101fdb9c7badb69714b
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-04 13:16:17 -0500

    Clean up aboutme file

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -15,14 +15,14 @@ export default function AboutMe() {
   
         <main className={utilStyles.main}>
             <h1 className={utilStyles.title}>
-            About Caitlin
+                About Caitlin
             </h1>
     
             <p className={utilStyles.description}>
-            <img className={styles.pic} src="/images/banff.jpg"></img>
-            <img className={styles.pic} src="/images/caitlinandgrant.jpg"></img>
-            <img className={styles.pic} src="/images/caitlinonmisty.jpg"></img>
-            <img className={styles.pic} src="/images/catsnme.jpg"></img>
+                <img className={styles.pic} src="/images/banff.jpg"></img>
+                <img className={styles.pic} src="/images/caitlinandgrant.jpg"></img>
+                <img className={styles.pic} src="/images/caitlinonmisty.jpg"></img>
+                <img className={styles.pic} src="/images/catsnme.jpg"></img>
             </p>
 
             <p className={styles.aboutMe}>
@@ -32,7 +32,7 @@ export default function AboutMe() {
             </p>
 
             <h1 className={utilStyles.title}>
-            Skills
+                Skills
             </h1>
 
             <div className={styles.technologies}>
@@ -73,65 +73,6 @@ export default function AboutMe() {
                     <h5>Sequelize</h5>
                 </div>
             </div>
-            
-
-            {/* <h1 className={utilStyles.title}>
-            Education
-            </h1>
-
-            <div className={styles.education}>
-            <ul className={styles.list}>
-                <li className={styles.card}>
-                    <h2 className={styles.title}>Full-Stack Software Development Certificate</h2>
-                    <h3 className={styles.organization}>
-                        <a href="https://www.digitalcrafts.com/" target="_blank">DigitalCrafts</a>
-                    </h3>
-                    <h3 className={styles.location}>Atlanta, GA</h3>
-                    <h3 className={styles.date}>July 2020 - Jan 2021</h3>
-                </li>
-                <li className={styles.card}>
-                    <h2 className={styles.title}>M.S. Environmental Planning and Design</h2>
-                    <h3 className={styles.organization}>
-                        <a href="https://ced.uga.edu/programs/mupd/" target="_blank">Unversity of Georgia</a>
-                    </h3>
-                    <h3 className={styles.location}>Athens, GA</h3>
-                    <h3 className={styles.date}>June 2016 - August 2018</h3>
-                </li>
-                <li className={styles.card}>
-                    <h2 className={styles.title}>B.S. Mathematics</h2>
-                    <h3 className={styles.organization}>
-                        <a href="https://www.berry.edu/academics/majors/mathematics/" target="_blank">Berry College</a>
-                    </h3>
-                    <h3 className={styles.location}>Rome, GA</h3>
-                    <h3 className={styles.date}>Aug 2013 - May 2015</h3>
-                </li>
-            </ul>
-            </div> 
-
-            <h1 className={utilStyles.title}>
-            Work Experience
-            </h1>
-
-            <div className={styles.work}>
-            <ul className={styles.list}>
-                <li className={styles.card}>
-                    <h2 className={styles.title}>Planning Analyst</h2>
-                    <h3 className={styles.organization}>
-                        <a href="https://www.chcrpa.org/" target="_blank">Chattanooga-Hamilton County Regional Planning Agency</a>
-                    </h3>
-                    <h3 className={styles.location}>Chattanooga, TN</h3>
-                    <h3 className={styles.date}>Aug 2019 - Present</h3>
-                </li>
-                <li className={styles.card}>
-                    <h2 className={styles.title}>Urban Planner</h2>
-                    <h3 className={styles.organization}>
-                        <a href="http://www.johngallup.com/" target="_blank">John Gallup and Associates</a>
-                    </h3>
-                    <h3 className={styles.location}>Marietta, GA</h3>
-                    <h3 className={styles.date}>Sept 2018 - June 2019</h3>
-                </li>
-            </ul>
-            </div>  */}
 
             <a className={styles.resume} href="./images/Crawford-Resume-Nov2020.pdf" target="_blank">
             Check out my full resume! 
